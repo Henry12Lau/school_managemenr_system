e130b12c5c22d71345e3cae0901ca2540bf94f57
louislaw93 <louis.official.work@gmail.com>
2024-08-27 19:21:11 +0800

    new table attendance

--- database/schema.sql
+++ database/schema.sql
@@ -13,6 +13,30 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- Dumping structure for table public.attendance
+CREATE TABLE IF NOT EXISTS "attendance" (
+	"id" SERIAL NOT NULL,
+	"subject_class_schedule_id" INTEGER NULL DEFAULT NULL,
+	"student_id" INTEGER NULL DEFAULT NULL,
+	"create_date" TIMESTAMP NULL DEFAULT now(),
+	"create_id" INTEGER NULL DEFAULT NULL,
+	"update_date" TIMESTAMP NULL DEFAULT now(),
+	"update_id" INTEGER NULL DEFAULT NULL,
+	"is_deleted" BOOLEAN NULL DEFAULT false,
+	PRIMARY KEY ("id")
+);
+
+-- Dumping data for table public.attendance: 4 rows
+DELETE FROM "attendance";
+/*!40000 ALTER TABLE "attendance" DISABLE KEYS */;
+INSERT INTO "attendance" ("id", "subject_class_schedule_id", "student_id", "create_date", "create_id", "update_date", "update_id", "is_deleted") VALUES
+	(1, 1, 1, '2024-08-27 17:08:51.391991', NULL, '2024-08-27 17:09:09.092801', 1, 'true'),
+	(2, 1, 1, '2024-08-27 17:09:09.093305', NULL, '2024-08-27 17:09:53.058138', 1, 'true'),
+	(3, 1, 1, '2024-08-27 17:09:53.060899', NULL, '2024-08-27 17:15:57.27233', 1, 'true'),
+	(5, 2, 1, '2024-08-27 17:30:02.201514', 1, '2024-08-27 17:30:02.201514', 1, 'false'),
+	(4, 1, 1, '2024-08-27 17:16:07.283065', NULL, '2024-08-27 17:30:09.486008', 1, 'true');
+/*!40000 ALTER TABLE "attendance" ENABLE KEYS */;
+
 -- Dumping structure for table public.course
 CREATE TABLE IF NOT EXISTS "course" (
 	"id" SERIAL NOT NULL,
@@ -28,7 +52,7 @@ CREATE TABLE IF NOT EXISTS "course" (
 	PRIMARY KEY ("id")
 );
 
--- Dumping data for table public.course: -1 rows
+-- Dumping data for table public.course: 1 rows
 DELETE FROM "course";
 /*!40000 ALTER TABLE "course" DISABLE KEYS */;
 INSERT INTO "course" ("id", "course_code", "course_name", "duration", "category", "create_date", "create_id", "update_date", "update_id", "is_deleted") VALUES
@@ -144,7 +168,7 @@ CREATE TABLE IF NOT EXISTS "subject" (
 	PRIMARY KEY ("id")
 );
 
--- Dumping data for table public.subject: -1 rows
+-- Dumping data for table public.subject: 2 rows
 DELETE FROM "subject";
 /*!40000 ALTER TABLE "subject" DISABLE KEYS */;
 INSERT INTO "subject" ("id", "subject_no", "subject_name", "create_date", "create_id", "update_date", "update_name", "is_deleted", "number_of_lesson", "lesson_duration") VALUES
@@ -169,6 +193,10 @@ CREATE TABLE IF NOT EXISTS "subject_class" (
 -- Dumping data for table public.subject_class: 1 rows
 DELETE FROM "subject_class";
 /*!40000 ALTER TABLE "subject_class" DISABLE KEYS */;
+INSERT INTO "subject_class" ("id", "subject_id", "start_date", "create_date", "create_id", "update_date", "update_id", "is_deleted", "staff_id") VALUES
+	(3, 2, '2024-09-01 17:46:06', '2024-08-09 17:46:40.804003', 1, '2024-08-09 17:46:40.804003', 1, 'false', 1);
+/*!40000 ALTER TABLE "subject_class" ENABLE KEYS */;
+
 -- Dumping structure for table public.subject_class_resources
 CREATE TABLE IF NOT EXISTS "subject_class_resources" (
 	"id" SERIAL NOT NULL,
@@ -186,7 +214,7 @@ CREATE TABLE IF NOT EXISTS "subject_class_resources" (
 	PRIMARY KEY ("id")
 );
 
--- Dumping data for table public.subject_class_resources: -1 rows
+-- Dumping data for table public.subject_class_resources: 0 rows
 DELETE FROM "subject_class_resources";
 /*!40000 ALTER TABLE "subject_class_resources" DISABLE KEYS */;
 /*!40000 ALTER TABLE "subject_class_resources" ENABLE KEYS */;
@@ -205,9 +233,12 @@ CREATE TABLE IF NOT EXISTS "subject_class_schedule" (
 	PRIMARY KEY ("id")
 );
 
--- Dumping data for table public.subject_class_schedule: -1 rows
+-- Dumping data for table public.subject_class_schedule: 2 rows
 DELETE FROM "subject_class_schedule";
 /*!40000 ALTER TABLE "subject_class_schedule" DISABLE KEYS */;
+INSERT INTO "subject_class_schedule" ("id", "subject_class_id", "lesson_start", "lesson_end", "create_date", "create_id", "update_date", "update_id", "is_deleted") VALUES
+	(1, 3, '2024-08-20 18:30:00', '2024-08-20 21:30:00', '2024-08-26 23:35:30.988469', 1, '2024-08-26 23:35:30.988469', 1, 'false'),
+	(2, 3, '2024-08-26 18:30:00', '2024-08-26 21:30:00', '2024-08-26 23:35:30.988469', 1, '2024-08-26 23:35:30.988469', 1, 'false');
 /*!40000 ALTER TABLE "subject_class_schedule" ENABLE KEYS */;
 
 -- Dumping structure for table public.subject_class_student
@@ -226,6 +257,10 @@ CREATE TABLE IF NOT EXISTS "subject_class_student" (
 -- Dumping data for table public.subject_class_student: -1 rows
 DELETE FROM "subject_class_student";
 /*!40000 ALTER TABLE "subject_class_student" DISABLE KEYS */;
+INSERT INTO "subject_class_student" ("id", "subject_class_id", "student_id", "create_date", "create_id", "update_date", "update_id", "is_deleted") VALUES
+	(1, 3, 1, '2024-08-09 17:49:16.11785', 1, '2024-08-09 17:49:16.11785', 1, 'false');
+/*!40000 ALTER TABLE "subject_class_student" ENABLE KEYS */;
+
 -- Dumping structure for table public.title
 CREATE TABLE IF NOT EXISTS "title" (
 	"id" SERIAL NOT NULL,
@@ -244,7 +279,7 @@ DELETE FROM "title";
 INSERT INTO "title" ("id", "title_name", "create_date", "create_id", "update_date", "update_id", "is_deleted") VALUES
 	(1, 'admin', '2024-08-09 17:38:51.93205', 1, '2024-08-09 17:38:51.93205', 1, 'false'),
 	(2, 'officer', '2024-08-09 17:39:41.403621', 1, '2024-08-09 17:39:41.403621', 1, 'false'),
-	(3, 'techer', '2024-08-09 17:40:41.293221', 1, '2024-08-09 17:40:41.293221', 1, 'false');
+	(3, 'teacher', '2024-08-09 17:40:41.293221', 1, '2024-08-09 17:40:41.293221', 1, 'false');
 /*!40000 ALTER TABLE "title" ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
