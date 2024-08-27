7d8d3e8a0221bf97388d49a172133e169c80a610
louislaw93 <louis.official.work@gmail.com>
2024-08-27 12:46:39 +0800

    new api staff subject (for Calendar)

--- backend/controllers/subjectClassController.js
+++ backend/controllers/subjectClassController.js
@@ -57,3 +57,23 @@ exports.getByStudent = async (req, res) => {
     res.status(500).json({ message: "Error" });
   }
 };
+exports.getByStaff = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const { rows } = await client.query(
+      `
+      SELECT sc.id AS subject_class_id, sc.subject_id, st.id AS staff_id, s.subject_no, s.subject_name, st.staff_no, st.surname, st.given_name, st.sex, st.tel, sch.lesson_start, sch.lesson_end
+      FROM subject_class sc
+      LEFT JOIN subject s ON s.id = sc.subject_id AND s.is_deleted = FALSE
+      LEFT JOIN staff st ON st.id = sc.staff_id  AND st.is_deleted = FALSE
+      LEFT JOIN subject_class_schedule sch ON sch.subject_class_id = sc.id AND sch.is_deleted = FALSE
+      WHERE st.id = $1 AND sc.is_deleted = FALSE
+      `,
+      [id]
+    );
+    return res.json({ classes: rows, message: "Success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error" });
+  }
+};
--- backend/routes/staffRoutes.js
+++ backend/routes/staffRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const staffController = require('../controllers/staffController');
+const subjectClassController = require('../controllers/subjectClassController');
 
 router.post('/', staffController.get);
+router.post('/schedule', subjectClassController.getByStaff);
 router.post('/resetPassword', staffController.resetPassword);
 
 module.exports = router;
